19dd601818d8ffe4090254c41c0f5179e22c5f74
rulyadhika <rulihanif@gmail.com>
2020-08-05 14:58:25 +0700

    making menu-card more responsive

--- js/order-food.js
+++ js/order-food.js
@@ -35,21 +35,26 @@ function filterData(data) {
 
 function displayData(data) {
   return ` <div class="col-md-4 pl-2 pr-2">
-                <div class="card my-2">
-                <img src="${
-                  data.gambar
-                }" class="card-img-top img-fluid" alt="...">
+                <div class="card my-2 menu-card-box">
+                <img src="${data.gambar}" class="card-img-top" alt="...">
                     <div class="card-body menu-card-info">
                         <h5 class="card-title">${data.nama}</h5>
                         <ul>
-                            <li>Harga : Rp. ${data.harga}</li>
-                            <li>${
+                            <li><span class="priceTitle">Harga : </span>Rp. ${
+                              data.harga
+                            }</li>
+                            <li class="descForMoreThan768px">${
                               data.deskripsi.length > 120
                                 ? `${data.deskripsi.substring(0, 117) + "..."}`
                                 : `${data.deskripsi}`
                             }</li>
+                            <li class="descForLessThan768px">${
+                              data.deskripsi.length > 50
+                                ? `${data.deskripsi.substring(0, 47) + "..."}`
+                                : `${data.deskripsi}`
+                            }</li>
                         </ul>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <button class="btn btn-primary add-to-cart-btn">Add To Cart</button>
                     </div>
                 </div>
             </div>`;
