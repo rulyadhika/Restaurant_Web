336e6c4bddde65c23d73adccff1e54205c1e36a2
rulyadhika <rulihanif@gmail.com>
2020-08-17 21:13:44 +0700

    adding navbar, image carousel,caption for carousel with type writer and footer for outlet.html

---
+++ js/outlet.js
@@ -0,0 +1,29 @@
+window.addEventListener("scroll", function () {
+  let navbarCustom = document.querySelector(".navbar-custom");
+  if (window.pageYOffset == 0) {
+    navbarCustom.classList.remove("navbar-toggle");
+  } else if (window.pageYOffset > 60) {
+    navbarCustom.classList.add("navbar-toggle");
+  }
+});
+
+function typeWriter() {
+  const text = "RadhikaCuisine - Outlet";
+  let i = 0;
+  const captionForCarousel = document.querySelector(".caption-for-carousel");
+  captionForCarousel.innerHTML = "";
+  setInterval(() => {
+    if (i < text.length) {
+      captionForCarousel.innerHTML += text[i];
+      i++;
+      if (i == text.length) {
+        setTimeout(() => {
+          captionForCarousel.innerHTML = "";
+          i = 0;
+        }, 5000);
+      }
+    }
+  }, 100);
+}
+
+typeWriter();
