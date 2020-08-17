9a46610dee793cf930c1adff055493f06a3d94c1
rulyadhika <rulihanif@gmail.com>
2020-08-17 23:04:46 +0700

    adding outlet.json file and fill it with dummy data, adding function to get outlet data, and displaying outlet data when the page get loaded

--- js/outlet.js
+++ js/outlet.js
@@ -1,5 +1,45 @@
+function getData() {
+  return fetch("json/outlet.json")
+    .then((result) => {
+      if (!result.ok) {
+        throw new Error("Something went wrong");
+      } else {
+        return result.json();
+      }
+    })
+    .then((result) => result);
+}
+
+window.addEventListener("load", async function () {
+  try {
+    const data = await getData();
+    const sortedData = data.sort((a, b) => a.kota.localeCompare(b.kota));
+    displayData(sortedData);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+function displayData(data) {
+  const tableBody = document.querySelector(".table-body");
+  let count = 1;
+  const allData = data
+    .map(
+      (data) => `<tr>
+    <th scope="row" class="number">${count++}</th>
+    <td class="kota">${data.kota}</td>
+    <td class="alamat">
+      ${data.alamat} <br />
+      <span>(No.Telp : ${data.telp})</span>
+    </td>
+  </tr>`
+    )
+    .join("");
+  tableBody.innerHTML = allData;
+}
+
 window.addEventListener("scroll", function () {
-  let navbarCustom = document.querySelector(".navbar-custom");
+  const navbarCustom = document.querySelector(".navbar-custom");
   if (window.pageYOffset == 0) {
     navbarCustom.classList.remove("navbar-toggle");
   } else if (window.pageYOffset > 60) {
